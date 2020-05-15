622be375de61988dcbfd6a0f601fb20301ea3e26
Francisco Giordano <frangio.1@gmail.com>
2020-05-15 14:48:40 -0300

    add initial set of tests and fix schema so they pass

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-
-import { answer } from '.';
-
-test('answer', t => {
-  t.is(answer, 42);
-});
---
+++ test.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const Ajv = require('ajv');
+const lodash = require('lodash');
+
+const ajv = new Ajv({ verbose: true });
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+const validate = ajv.compile(require('./schema.json'));
+
+const dir = 'solidity/test/libsolidity/ASTJSON';
+
+// we read all jsons except those marked legacy
+const inputs = fs.readdirSync(dir).filter(e => /^.*(?<!_legacy)\.json$/.test(e));
+
+for (const f of inputs) {
+  if (f === 'documentation.json') continue; // invalid json here
+
+  it(f, function () {
+    const doc = require(path.resolve(dir, f));
+    if (!validate(doc)) {
+      const longest = lodash.maxBy(validate.errors, e => e.dataPath.split('.').length);
+      throw new Error(formatError(longest, doc));
+    }
+  });
+}
+
+function formatError(error, doc) {
+  const { params, message, data, dataPath } = error;
+  const path = dataPath.slice(1);
+
+  const pathComponents = path.split('.');
+  const nodeTypes = pathComponents.map((_, i) => {
+    const subPath = pathComponents.slice(0, i).concat('nodeType').join('.');
+    return lodash.get(doc, subPath);
+  }).join(' > ');
+
+  fs.writeFileSync('failed.json', JSON.stringify(doc));
+
+  return JSON.stringify({
+    message,
+    params,
+    dataPath,
+    nodeTypes,
+    data,
+  }, null, 2);
+}
