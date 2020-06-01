6613ae4a8fd6ec0f4552fe64767b195f4c886c5f
Francisco Giordano <frangio.1@gmail.com>
2020-06-01 12:34:27 -0300

    add tests with locally compiled contract

---
+++ demo.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract Foo {
+  uint x = 2 >> 1;
+}
--- test.js
+++ test.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+const fs = require('promisified/fs');
 const path = require('path');
+const https = require('https');
+const stream = require('promisified/stream');
 const Ajv = require('ajv');
 const lodash = require('lodash');
 const chalk = require('chalk');
 const util = require('util');
+const solcWrapper = require('solc/wrapper');
+const assert = require('assert');
 
 const ajv = new Ajv({ verbose: true });
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
 const validate = ajv.compile(require('./schema.json'));
+
+function assertValid(ast) {
+  if (!validate(ast)) {
+    const longest = lodash.maxBy(validate.errors, e => e.dataPath.split('.').length);
+    throw new Error(formatError(longest, ast));
+  }
+}
 
 describe('json samples from solidity repository', function () {
   const dir = 'solidity/test/libsolidity/ASTJSON';
@@ -20,10 +31,7 @@ describe('json samples from solidity repository', function () {
     // Some of these files are arrays so we use concat to treat them uniformly.
     for (const ast of [].concat(doc)) {
       it(f, function () {
-        if (!validate(ast)) {
-          const longest = lodash.maxBy(validate.errors, e => e.dataPath.split('.').length);
-          throw new Error(formatError(longest, ast));
-        }
+        assertValid(ast);
       });
     }
   }
@@ -34,13 +42,55 @@ describe('openzeppelin contracts', function () {
 
   for (const [ path, { ast } ] of Object.entries(sources)) {
     it(path, function () {
-      if (!validate(ast)) {
-        const longest = lodash.maxBy(validate.errors, e => e.dataPath.split('.').length);
-        throw new Error(formatError(longest, ast));
-      }
+      assertValid(ast);
     });
   }
 });
+
+describe('demo contract with solc wasm', function () {
+  const versions = ['v0.6.8+commit.0bbfe453'];
+
+  for (const version of versions) {
+    it(version, async function () {
+      this.timeout(0);
+      const solc = await requireSolc(version);
+      const output = JSON.parse(
+        solc.compile(
+          JSON.stringify({
+            language: 'Solidity',
+            sources: {
+              'demo.sol': {
+                content: await fs.readFile('./demo.sol', 'utf8'),
+              },
+            },
+            settings: {
+              outputSelection: { '*': { '': ['ast'] } },
+            },
+          })
+        )
+      );
+      assert.ifError(output.errors);
+      assertValid(output.sources['demo.sol'].ast);
+    });
+  }
+});
+
+async function requireSolc(version) {
+  const fileName = `soljson-${version}.js`;
+  const url = `https://solc-bin.ethereum.org/wasm/${fileName}`;
+  const dir = '.solc-bin';
+  const solc = path.resolve(dir, fileName)
+
+  if (!await fs.exists(solc)) {
+    console.error(`downloading solc ${version}...`);
+    await fs.mkdir(dir, { recursive: true });
+    const solcFile = fs.createWriteStream(solc);
+    https.get(url, res => res.pipe(solcFile));
+    await stream.finished(solcFile);
+  }
+
+  return solcWrapper(require(solc));
+}
 
 function formatError(error, doc) {
   const pathComponents = error.dataPath.split('.');
