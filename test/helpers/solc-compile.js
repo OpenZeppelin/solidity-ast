f916f7232b34ce833c426adc33698a0f592e5160
Francisco Giordano <frangio.1@gmail.com>
2020-12-17 13:03:46 -0300

    fix tests exit delay

--- test/helpers/solc-compile.js
+++ test/helpers/solc-compile.js
@@ -6,6 +6,12 @@ async function compile(version, sources) {
   const child = proc.fork(require.resolve('./solc-compile-helper'));
   child.send({ version, sources });
   const [output] = await events.once(child, 'message');
+
+  // allows tests to exit as soon as they finish
+  // otherwise solc module delays exit
+  child.disconnect();
+  child.unref();
+
   if (output.errors && output.errors.some(e => e.severity !== 'warning')) {
     throw new Error(lodash.map(output.errors, 'formattedMessage').join('\n'));
   }
