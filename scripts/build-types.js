69a9657b79f68c50cf27759e85da7b54bdbcf577
Francisco Giordano <frangio.1@gmail.com>
2021-05-22 20:07:34 -0300

    rewrite build-types.sh in javascript

---
+++ scripts/build-types.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const json2ts = require('json-schema-to-typescript');
+const fs = require('fs');
+
+const schema = require('../schema');
+
+json2ts.compile(schema, schema.title, {
+  strictIndexSignatures: true,
+  bannerComment: '/* tslint:disable */',
+}).then(result => {
+  fs.writeFileSync('types.d.ts', result);
+}).catch(err => {
+  console.error(err.stack);
+  process.exit(1);
+});
--- scripts/build-types.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-json2ts schema.json types.d.ts --strictIndexSignatures --bannerComment "/* tslint:disable */"
