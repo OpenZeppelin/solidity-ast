b03fe0543e6f85d2a807790162c636d82084d789
Francisco Giordano <frangio.1@gmail.com>
2022-04-24 18:45:36 +0200

    validate solc version

--- test/helpers/solc-compile-helper.js
+++ test/helpers/solc-compile-helper.js
@@ -6,16 +6,31 @@ const events = require('events');
 
 process.once('message', async ({ version, sources }) => {
   const solc = require(`solc-${version}`);
-  const output = JSON.parse(
-    solc.compile(
-      JSON.stringify({
-        sources,
-        language: 'Solidity',
-        settings: {
-          outputSelection: { '*': { '': ['ast'] } },
-        },
-      })
-    )
-  );
+
+  // version() returns something like '0.8.13+commit.abaa5c0e.Emscripten.clang'
+  const [trueVersion] = solc.version().split('+');
+
+  let output;
+
+  if (trueVersion !== version) {
+    output = {
+      errors: [
+        { formattedMessage: `Package solc-${version} is actually solc@${trueVersion}` },
+      ],
+    };
+  } else {
+    output = JSON.parse(
+      solc.compile(
+        JSON.stringify({
+          sources,
+          language: 'Solidity',
+          settings: {
+            outputSelection: { '*': { '': ['ast'] } },
+          },
+        })
+      )
+    );
+  }
+
   process.send(output);
 });
