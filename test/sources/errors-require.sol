805a75b00ced61c431dbba091f5fd2e030f3ba25
svc-ghb-is-04 <160617330+svc-ghb-is-04@users.noreply.github.com>
2024-05-27 21:32:37 +0200

    Update solc (#42)
    Co-authored-by: Francisco Giordano <fg@frang.io>

---
+++ test/sources/errors-require.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.26;
+
+/// @dev docs
+error InsufficientBalance(uint available, uint required);
+
+contract Foo {
+    function bar(uint x) external {
+        require(x == 0, InsufficientBalance({ available: 0, required: 0 }));
+    }
+}
