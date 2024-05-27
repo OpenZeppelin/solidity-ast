cd86934238efd0e54cc2b6889473c6a14d4c5189
Francisco <frangio.1@gmail.com>
2024-05-27 16:31:15 -0300

    remove unused error

--- test/sources/errors-require.sol
+++ test/sources/errors-require.sol
@@ -6,8 +6,6 @@ pragma solidity >=0.8.26;
 error InsufficientBalance(uint available, uint required);
 
 contract Foo {
-    error OhNo();
-
     function bar(uint x) external {
         require(x == 0, InsufficientBalance({ available: 0, required: 0 }));
     }
