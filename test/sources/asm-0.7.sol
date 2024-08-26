991a44ffcccc8c3303568f04b47e07e401b81aa9
Francisco Giordano <fg@frang.io>
2024-08-26 11:13:35 -0300

    remove unused arg

--- test/sources/asm-0.7.sol
+++ test/sources/asm-0.7.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7;
 contract Asm {
     uint s;
 
-    function foo(bytes calldata b) external {
+    function foo() external {
         assembly {
             let x := s.slot
             let y := s.offset
