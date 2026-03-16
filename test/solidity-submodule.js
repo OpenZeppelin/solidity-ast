fee3757c71bd94fe621033711e4089103dc25c9c
Francisco Giordano <fg@frang.io>
2026-03-15 22:12:40 -0300

    replace mocha with node:test

--- test/solidity-submodule.js
+++ test/solidity-submodule.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
+const { test } = require('node:test');
 
 const { assertValid } = require('./helpers/assert-valid');
 
 const dir = path.join(__dirname, 'solidity/test/libsolidity/ASTJSON');
 
-describe('solidity submodule', function () {
+test('solidity submodule', async function (t) {
+  assert(fs.existsSync(dir), 'initialize the test/solidity submodule to run this test');
   // we read all jsons except those marked legacy or parseOnly
   const inputs = fs.readdirSync(dir)
     .filter(e => /^.*(?<!_legacy|_parseOnly)\.json$/.test(e));
-
-  before('read inputs', async function () {
-    const contents = await Promise.all(inputs.map(f => fs.promises.readFile(path.resolve(dir, f), 'utf8')));
-    this.inputContents = {};
-    for (const [i, content] of contents.entries()) {
-      this.inputContents[inputs[i]] = content;
-    }
-  });
+  const contents = await Promise.all(inputs.map(f => fs.promises.readFile(path.resolve(dir, f), 'utf8')));
+  const inputContents = {};
+  for (const [i, content] of contents.entries()) {
+    inputContents[inputs[i]] = content;
+  }
 
   for (const f of inputs) {
-    it(f, function () {
-      const text = this.inputContents[f];
+    await t.test(f, function () {
+      const text = inputContents[f];
       if (text.length === 0) return;
       const doc = JSON.parse(text.replace(/%EVMVERSION%/g, JSON.stringify('berlin')));
       // Some of these files are arrays so we use concat to treat them uniformly.
--- test/utils.js
+++ test/utils.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 const assert = require('assert');
 const fc = require('fast-check');
+const { describe, it, test } = require('node:test');
 
 const { latest } = require('./helpers/solc-versions');
 const { compile } = require('./helpers/solc-compile');
@@ -31,7 +32,7 @@ describe('isNodeType', function () {
   });
 });
 
-describe('findAll', function () {
+test('findAll', { timeout: 10 * 60 * 1000 }, async function (t) {
   const counts = {
     Block: 1,
     ContractDefinition: 1,
@@ -53,154 +54,137 @@ describe('findAll', function () {
 
   const starCount = Object.values(counts).reduce((a, b) => a + b, 0);
 
-  before('reading and compiling source file', async function () {
-    this.timeout(10 * 60 * 1000);
-    const output = await compile(latest, {
-      ['find-all.sol']: { content: await fs.readFile(path.join(__dirname, 'sources/find-all.sol'), 'utf8') },
-      ['import.sol']: { content: await fs.readFile(path.join(__dirname, 'sources/import.sol'), 'utf8') },
-    });
-    this.ast = output.sources['find-all.sol'].ast;
+  const output = await compile(latest, {
+    ['find-all.sol']: { content: await fs.readFile(path.join(__dirname, 'sources/find-all.sol'), 'utf8') },
+    ['import.sol']: { content: await fs.readFile(path.join(__dirname, 'sources/import.sol'), 'utf8') },
   });
+  const { ast } = output.sources['find-all.sol'];
 
-  it('basic', function () {
+  await t.test('basic', function () {
     for (const nodeType in counts) {
-      const nodes = [...findAll(nodeType, this.ast)];
+      const nodes = [...findAll(nodeType, ast)];
       assert.strictEqual(nodes.length, counts[nodeType]);
     }
   });
 
-  it('curried', function () {
+  await t.test('curried', function () {
     const nodeType = 'StructDefinition';
-    const nodes = [...findAll(nodeType)(this.ast)];
+    const nodes = [...findAll(nodeType)(ast)];
     assert.strictEqual(nodes.length, counts[nodeType]);
   });
 
-  it('multiple', function () {
+  await t.test('multiple', function () {
     const nodeTypes = Object.keys(counts);
     fc.assert(
       fc.property(fc.shuffledSubarray(nodeTypes), nodeTypes => {
         const count = nodeTypes.map(t => counts[t]).reduce((a, b) => a + b, 0);
-        const nodes = [...findAll(nodeTypes, this.ast)];
+        const nodes = [...findAll(nodeTypes, ast)];
         assert.strictEqual(nodes.length, count);
       })
     );
   });
 
-  it('star', function () {
-    const nodes = [...findAll('*', this.ast)];
+  await t.test('star', function () {
+    const nodes = [...findAll('*', ast)];
     assert.strictEqual(nodes.length, starCount);
   });
 });
 
-describe('ast dereferencer', function () {
+test('ast dereferencer', { timeout: 10 * 60 * 1000 }, async function (t) {
   const source = path.join(__dirname, 'sources/ast-deref.sol');
+  const content = await fs.readFile(source, 'utf8');
+  const output = await compile(latest, { 0: { content } });
+  const ast = output.sources[0].ast;
 
-  before('reading and compiling source file', async function () {
-    this.timeout(10 * 60 * 1000);
-    const content = await fs.readFile(source, 'utf8');
-    this.output = await compile(latest, { 0: { content } });
-    this.ast = this.output.sources[0].ast;
-  });
-
-  it('finds contracts', function () {
-    const deref = astDereferencer(this.output);
-    for (const c of findAll('ContractDefinition', this.ast)) {
+  await t.test('finds contracts', function () {
+    const deref = astDereferencer(output);
+    for (const c of findAll('ContractDefinition', ast)) {
       assert.strictEqual(c, deref('ContractDefinition', c.id));
     }
   });
 
-  it('finds functions', function () {
-    const deref = astDereferencer(this.output);
-    for (const c of findAll('FunctionDefinition', this.ast)) {
+  await t.test('finds functions', function () {
+    const deref = astDereferencer(output);
+    for (const c of findAll('FunctionDefinition', ast)) {
       assert.strictEqual(c, deref('FunctionDefinition', c.id));
     }
   });
 
-  it('cache works', function () {
-    const deref = astDereferencer(this.output);
-    const [c1] = findAll('ContractDefinition', this.ast);
+  await t.test('cache works', function () {
+    const deref = astDereferencer(output);
+    const [c1] = findAll('ContractDefinition', ast);
     assert.strictEqual(c1, deref('ContractDefinition', c1.id));
     assert.strictEqual(c1, deref('ContractDefinition', c1.id));
   });
 
-  it('errors on wrong type', function () {
-    const deref = astDereferencer(this.output);
-    const [c1] = findAll('ContractDefinition', this.ast);
+  await t.test('errors on wrong type', function () {
+    const deref = astDereferencer(output);
+    const [c1] = findAll('ContractDefinition', ast);
     assert.throws(
       () => deref('FunctionDefinition', c1.id),
       { message: /^No node with id \d+ of type FunctionDefinition$/ },
     );
   });
 
-  it('errors on unknown id', function () {
-    const deref = astDereferencer(this.output);
+  await t.test('errors on unknown id', function () {
+    const deref = astDereferencer(output);
     assert.throws(
       () => deref('FunctionDefinition', 1e10),
       { message: /^No node with id \d+ of type FunctionDefinition$/ },
     );
   });
 
-  it('multiple node types', function () {
-    const deref = astDereferencer(this.output);
-    const [c1] = findAll('ContractDefinition', this.ast);
-    const [f1] = findAll('FunctionDefinition', this.ast);
+  await t.test('multiple node types', function () {
+    const deref = astDereferencer(output);
+    const [c1] = findAll('ContractDefinition', ast);
+    const [f1] = findAll('FunctionDefinition', ast);
     assert.strictEqual(c1, deref(['ContractDefinition', 'FunctionDefinition'], c1.id));
     assert.strictEqual(f1, deref(['ContractDefinition', 'FunctionDefinition'], f1.id));
   });
 
-  it('curried', function () {
-    const deref = astDereferencer(this.output);
-    const c3 = [...findAll('ContractDefinition', this.ast)].find(c => c.name === 'C3');
+  await t.test('curried', function () {
+    const deref = astDereferencer(output);
+    const c3 = [...findAll('ContractDefinition', ast)].find(c => c.name === 'C3');
     const baseContracts = c3.linearizedBaseContracts.map(deref('ContractDefinition'));
     assert.deepEqual(baseContracts.map(c => c.name), ['C3', 'C2', 'C1']);
   });
 });
 
-describe('ast dereferencer with source unit', function () {
+test('ast dereferencer with source unit', { timeout: 10 * 60 * 1000 }, async function (t) {
   const source0 = path.join(__dirname, 'sources/ast-deref.sol');
   const source1 = path.join(__dirname, 'sources/ast-deref-2.sol');
+  const content0 = await fs.readFile(source0, 'utf8');
+  const content1 = await fs.readFile(source1, 'utf8');
+  const output = await compile(latest, { 0: { content: content0 }, 1: { content: content1 } });
 
-  before('reading and compiling source file', async function () {
-    this.timeout(10 * 60 * 1000);
-    const content0 = await fs.readFile(source0, 'utf8');
-    const content1 = await fs.readFile(source1, 'utf8');
-    this.output = await compile(latest, { 0: { content: content0 }, 1: { content: content1 } });
-    this.ast = this.output.sources[0].ast;
-  });
-
-
-  it('finds contracts', function () {
-    const deref = astDereferencer(this.output);
-    for (const { ast } of Object.values(this.output.sources)) {
-      for (const c of findAll('ContractDefinition', ast)) {
+  await t.test('finds contracts', function () {
+    const deref = astDereferencer(output);
+    for (const { ast: astSourceUnit } of Object.values(output.sources)) {
+      for (const c of findAll('ContractDefinition', astSourceUnit)) {
         const { node, sourceUnit } = deref.withSourceUnit('ContractDefinition', c.id);
         assert.strictEqual(c, node);
-        assert.strictEqual(sourceUnit, ast);
+        assert.strictEqual(sourceUnit, astSourceUnit);
       }
     }
   });
 });
 
-describe('src decoder', function () {
+test('src decoder', { timeout: 10 * 60 * 1000 }, async function (t) {
   const source = path.join(__dirname, 'sources/src-decoder.sol');
+  const content = await fs.readFile(source, 'utf8');
+  const input = { sources: { 'file.sol': { content } } };
+  const output = await compile(latest, input.sources);
+  const ast = output.sources['file.sol'].ast;
 
-  before('reading and compiling source file', async function () {
-    this.timeout(10 * 60 * 1000);
-    const content = await fs.readFile(source, 'utf8');
-    this.input = { sources: { 'file.sol': { content } } };
-    this.output = await compile(latest, this.input.sources);
-    this.ast = this.output.sources['file.sol'].ast;
-  });
-
-  it('ascii', function () {
-    const decodeSrc = srcDecoder(this.input, this.output);
-    const line5 = [...findAll('ContractDefinition', this.ast)].find(c => c.name === 'Line5');
+  await t.test('ascii', function () {
+    const decodeSrc = srcDecoder(input, output);
+    const line5 = [...findAll('ContractDefinition', ast)].find(c => c.name === 'Line5');
     assert.strictEqual(decodeSrc(line5), 'file.sol:5');
   });
 
-  it('multi-byte utf8', function () {
-    const decodeSrc = srcDecoder(this.input, this.output);
-    const line8 = [...findAll('ContractDefinition', this.ast)].find(c => c.name === 'Line8');
+  await t.test('multi-byte utf8', function () {
+    const decodeSrc = srcDecoder(input, output);
+    const line8 = [...findAll('ContractDefinition', ast)].find(c => c.name === 'Line8');
     assert.strictEqual(decodeSrc(line8), 'file.sol:8');
   });
 });
